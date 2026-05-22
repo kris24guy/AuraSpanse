9828a2c33bd74b8e37130fe3d1ad261df35eb0fb
kris24guy <kris24guy@gmail.com>
2026-05-22 10:47:01 -0700

    Initialize Express server with a basic route

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Aurascope backend is alive in the cloud');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Aurascope running on port', PORT);
+});
