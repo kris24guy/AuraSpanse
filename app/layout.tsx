e53454a8ad64e771113b716544d7f91fb221a83c
kris24guy <kris24guy@gmail.com>
2026-05-27 20:56:22 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{
+          margin: 0,
+          fontFamily: 'sans-serif',
+          background: 'black',
+        }}
+      >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
